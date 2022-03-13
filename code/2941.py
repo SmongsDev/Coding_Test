21dec4ce26b86f8311522552a1bfc82535e60c0c
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2022-03-14 01:13:54 +0900

    Create 2941.py

---
+++ code/2941.py
@@ -0,0 +1,12 @@
+s = input()
+
+s = s.replace("c=","1")
+s = s.replace("c-","1")
+s = s.replace("dz=","1")
+s = s.replace("d-","1")
+s = s.replace("lj","1")
+s = s.replace("nj","1")
+s = s.replace("s=","1")
+s = s.replace("z=","1")
+
+print(len(s))

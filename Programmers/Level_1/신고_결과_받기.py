b4822644bd4a4c94a7ae390d71ef467bcfff2cca
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2022-06-02 23:36:41 +0900

    22/06/02 멸망 코드

---
+++ Programmers/Level_1/신고_결과_받기.py
@@ -0,0 +1,19 @@
+def solution(id_list, report, k):
+    answer = []
+    resutlt = []
+    dic = {}
+    _dic = {}
+    re = [i.split(" ") for i in set(report)]
+    for i in range(len(id_list)):
+        dic[id_list[i]] = 0
+    for i in range(len(id_list)):
+        _dic[id_list[i]] = 0
+    for i in re:
+        dic[i[1]] += 1
+    for i in dic:
+        if dic[i] >= k:
+            answer.append(i)
+    for i in re:
+        if i[1] in answer:
+            _dic[i[0]] += 1
+    return [j for i, j in _dic.items()]

267461fe25facdaabd158437e35f8d691bcb0d21
Mollang <62112908+javascriptKR@users.noreply.github.com>
2023-01-13 19:23:14 +0900

    01/13 backjoon Hashing

--- BackJoon/15829.py
+++ BackJoon/15829.py
@@ -1,2 +1,9 @@
-# Hashing
+import sys
 
+l = int(sys.stdin.readline())
+string = sys.stdin.readline().rstrip()
+result = 0
+for i in range(l):
+    Ascii = ord(string[i]) - 96
+    result += Ascii * pow(31,i)
+print(result % 1234567891)

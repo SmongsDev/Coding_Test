24582b33e63d783118e670459e8f882e7afe3a08
Mollang <62112908+javascriptKR@users.noreply.github.com>
2023-11-27 14:46:59 +0000

    11/27 Solved

---
+++ BaeckJoon/SW/11053.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [arr[0]]
+for i in range(1,len(arr)):
+    if arr[i] > dp[-1]:
+        dp.append(arr[i])
+print(len(dp))
+
+# https://lgphone.tistory.com/129

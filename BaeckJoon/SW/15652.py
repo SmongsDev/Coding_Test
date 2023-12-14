7f72586f2822e15af2dd62ce733198fd63bd19ff
Mollang <62112908+javascriptKR@users.noreply.github.com>
2023-12-14 15:00:06 +0000

    12/14 solved additional problem

--- BaeckJoon/SW/15652.py
+++ BaeckJoon/SW/15652.py
@@ -1,13 +1,22 @@
-n, m = map(int,input().split())
-arr = []
+from itertools import combinations_with_replacement
 
-def back(depth):
+n, m = map(int, input().split())
+nums = [ i for i in range(1,n+1) ]
+for i in combinations_with_replacement(nums,m):
+    for j in range(len(i)):
+        print(i[j], end=' ')
+    print()
+
+# 다른 풀이 
+arr = []
+def back():
     if len(arr) == m:
-        print(' '.join(map(str,arr)))
+        print(' '.join(map(str, arr)))
         return
     else:
-        for i in range(depth, n+1):
-            arr.append(i)
-            back(i)
-            arr.pop()
-back(1)
+        for i in range(1, n+1):
+            if len(arr) == 0 or arr[-1] <= i:
+                arr.append(i)
+                back()
+                arr.pop()
+back()
